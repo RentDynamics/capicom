60da61adf640e1ee9cbc52e2460570f8b76f716e
Skyler Cain <skylercain@gmail.com>
2019-07-15 10:45:45 -0600

    Initial commit of library

---
+++ tests/decryptor_tests.py
@@ -0,0 +1,11 @@
+import html
+import unittest
+
+from capicom.decrypt import Capicom3DESCipher
+
+
+class DecryptorTests(unittest.TestCase):
+
+    def test_decrypt_returns_decrypted_string(self):
+        pass
+
